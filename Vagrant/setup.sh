d80fd37a01a1137705df296afbc346c5c055d1db
Yuta Hayakawa <haya@risoluto.org>
2014-11-23 02:14:04 +0900

    First commit

---
+++ Vagrant/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export LANG=C
+
+# System Update
+yum -y update >& /var/log/vagrant_yum_update.log
+
+# Risoluto Deploy
+mkdir -p /root/gittmp
+cd /root/gittmp
+git clone https://github.com/Risoluto/Risoluto-Core.git
+rm -fr /var/www/public_html
+mv /root/gittmp/Risoluto-Core/* /var/www/.
+chown -R apache.apache /var/www/*
+cd /var/www/risoluto/lib/
+curl -sS https://getcomposer.org/installer | /usr/local/php5/bin/php
+/usr/local/php5/bin/php composer.phar install -o --dev
+chmod -R 777 /var/www/risoluto/data
+rm -fr /root/gittmp
